3e7c46009d39e98cd38bd07fad205fc51dcffad9
Tiago <tiagojardine@MacBook-Pro-de-admin.local>
2025-06-01 23:51:32 -0300

    Usar o ícone ChevronRight no botão da seção Quem sou eu

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import { ArrowRight } from 'lucide-react';
+import { ChevronRight } from 'lucide-react';
 
 const About = () => {
   return (
