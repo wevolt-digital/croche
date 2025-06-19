199cd322cd211789387aeca687d089081dcad163
Tiago <tiagojardine@MacBook-Pro-de-admin.local>
2025-06-18 21:41:01 -0300

    feat: adiciona geração automática de sitemap com next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://pricamposcroche.com.br',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: false,
+};
